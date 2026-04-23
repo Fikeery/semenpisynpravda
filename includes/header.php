a803d39c4b9085943b44291fb0fdc46607ac278d
KrytoiPro228 <privt@gmail.com>
2026-04-23 13:59:25 +0300

    Favicon

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pageTitle ?? 'Мой сайт' ?></title>
+    
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
 <?php
 $menuItems = [
     'Главная' => '/index.php',
@@ -5,6 +18,7 @@ $menuItems = [
     'Контакты' => '/pages/contact.php'
 ];
 ?>
+
 
 <header>
     <nav>
